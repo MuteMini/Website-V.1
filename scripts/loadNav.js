be8e77b244875f51000ac2e4013ab581a556d089
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-05-01 20:13:44 -0400

    Moved nav into JS file
    The sidebar navigation code is now moved into a javascript file. Makes code organized.

---
+++ scripts/loadNav.js
@@ -0,0 +1,20 @@
+// Sidebar learned from https://www.w3schools.com/howto/howto_js_sidenav.asp
+// learned document.write from https://stackoverflow.com/a/15250208
+document.write('\
+<a onclick="openNav()" href="javascript:void(0)" id="open">&equiv;</a>\
+<nav id="sidenav">\
+    <a onclick="closeNav()" href="javascript:void(0)" id="close">&times;</a>\
+    <a href="index.html">Home</a>\
+    <a href="about.html">About Me</a>\
+    <a href="projects.html">Projects</a>\
+    <a href="tutorialMain.html">Tutorials</a>\
+</nav>\
+<script>\
+    function openNav() {\
+        document.getElementById("sidenav").style.width = "250px";\
+    }\
+    function closeNav() {\
+        document.getElementById("sidenav").style.width = "0";\
+    }\
+</script>\
+');
